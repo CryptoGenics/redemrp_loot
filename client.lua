b02f1893f62ed2731e4cf67f0eab3abad42e64c9
CryptoGenics <45253987+CryptoGenics@users.noreply.github.com>
2020-04-30 10:34:49 -0500

    fixed to check for native usage

--- client.lua
+++ client.lua
@@ -9,12 +9,13 @@ Citizen.CreateThread(function()
 				Wait(0)
 				local player = PlayerPedId()
 				local coords = GetEntityCoords(player)
+				local entityHit = 0
 				local shapeTest = StartShapeTestBox(coords.x, coords.y, coords.z, 2.0, 2.0, 2.0, 0.0, 0.0, 0.0, true, 8, player)
 				local rtnVal, hit, endCoords, surfaceNormal, entityHit = GetShapeTestResult(shapeTest)
-				local isHuman = IsPedHuman(entityHit)
+				local type = GetPedType(entityHit)
 				local dead = IsEntityDead(entityHit)
 				local PressTime = 0
-				if isHuman and dead then
+				if type == 4 and dead then
 					local looted = Citizen.InvokeNative(0x8DE41E9902E85756, entityHit)
 					if not looted then
 						shape = false
@@ -22,16 +23,20 @@ Citizen.CreateThread(function()
 						PressTime = GetGameTimer()
 						while looting do
 							Wait(0)
-							local looted = Citizen.InvokeNative(0x8DE41E9902E85756, entityHit)
-							if IsControlJustReleased(0,1101824977) and not looted then
+							if IsControlJustReleased(0,1101824977) then
 								KeyHeldTime = GetGameTimer() - PressTime
 								PressTime = 0
 								if KeyHeldTime > 250 then
 									looting = false
-									TaskLootEntity(player, entityHit)
-									local loot = math.random(1, 10)
-									local lootpay = loot / 100
-									TriggerServerEvent('cryptos_loot', lootpay)
+									Wait(500)
+									local lootedcheck = Citizen.InvokeNative(0x8DE41E9902E85756, entityHit)
+									if lootedcheck then
+										local loot = math.random(1, 10)
+										local lootpay = loot / 100
+										TriggerServerEvent('cryptos_loot', lootpay)
+									else
+										looting = false
+									end
 								else
 									looting = false
 								end
